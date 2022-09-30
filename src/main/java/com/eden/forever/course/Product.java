53fe4e9c7f1fde262fa64a43f6cbb1a3ccae27d5
EdenMaisUmDia <ederlanbrito@gmail.com>
2022-09-30 17:09:27 -0300

    save

--- src/main/java/com/eden/forever/course/Product.java
+++ src/main/java/com/eden/forever/course/Product.java
@@ -12,15 +12,17 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable{
+public class Product implements Serializable {
 
-	
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -28,14 +30,15 @@ public class Product implements Serializable{
 	private String description;
 	private Double price;
 	private String imgURL;
-	
+
 	@ManyToMany
-	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"),
-	inverseJoinColumns = @JoinColumn(name = "category_id"))
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
-	
-	Product(){
-		
+
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
+	Product() {
+
 	}
 
 	public Product(Long id, String name, String description, Double price, String imgURL) {
@@ -92,6 +95,15 @@ public class Product implements Serializable{
 		return Objects.hash(id);
 	}
 
+	@JsonIgnore
+	public Set<Order> getOrders() {
+		Set<Order> set = new HashSet<>();
+		for (OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		return set;
+	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -107,6 +119,5 @@ public class Product implements Serializable{
 	public Set<Category> getCategories() {
 		return categories;
 	}
-	
-	
+
 }
